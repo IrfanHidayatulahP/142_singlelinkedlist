1a49c20a520230085f8ff742ace39bf5af5a0c74
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-05-18 15:22:11 +0700

    menambah case 1

--- 142_singlelinkedlist/142_singlelinkedlist/142_singlelinkedlist.cpp
+++ 142_singlelinkedlist/142_singlelinkedlist/142_singlelinkedlist.cpp
@@ -133,6 +133,15 @@ int main() {
 			cout << "5. Keluar" << endl;
 			cout << "Pilihan: ";
 			cin >> pilihan;
+			switch (pilihan) {
+			case 1:
+				addNode();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("Pause");
+				system("cls");
+				break;
+
+			}
 		}
 	}
 }
